5e8a240d59c739f9eeed0ba7d24ed8da4d4e36ad
Daniel Barclay <barclaysd@me.com>
2019-02-07 17:28:21 +0000

    Linear gradient for mobile background

--- mobile-frontend/App.js
+++ mobile-frontend/App.js
@@ -1,39 +1,33 @@
 import React, {Component} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to drinKing!</Text>
-      </View>
+        <LinearGradient colors={['#43C6AC', '#191654']} style={styles.linearGradient}>
+          <Text style={styles.buttonText}>
+            Sign in with Facebook
+          </Text>
+        </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  linearGradient: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
   },
-  welcome: {
-    fontSize: 20,
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  }
 });
