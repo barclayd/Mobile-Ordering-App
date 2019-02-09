dea74de63eaa952f752a4908ac6916dd254461a6
Daniel Barclay <barclaysd@me.com>
2019-02-09 22:27:24 +0000

    React Native Navigation and React Native Vector Icons added. Side Drawers for app navigation

---
+++ mobile-frontend/src/screens/ViewDrinks/ViewDrinks.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class ViewDrinks extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>View our best selling drinks</Text>
+            </View>
+        )
+    }
+};
+
+export default ViewDrinks;
