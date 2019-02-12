9876003ca851c579b0a9896477fd37a9f60665bf
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-02-12 21:09:51 +0000

    adding Font Awesome to xocde project

--- mobile/src/screens/WelcomeScreen/WelcomeScreen.js
+++ mobile/src/screens/WelcomeScreen/WelcomeScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {View, TextInput, StyleSheet, Dimensions, ImageBackground,} from 'react-native';
 import LinearGradient from "react-native-linear-gradient";
-import barImage from '../../assets/barConfetti.jpg';
+import barImage from '../../assests/barConfetti.jpg';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import * as colours from '../../styles/colourScheme';
 
 class WelcomeScreen extends Component {
@@ -9,13 +10,26 @@ class WelcomeScreen extends Component {
     render() {
 
         return (
-            <ImageBackground style={styles.backgroundImg} source={barImage}>
-                <LinearGradient colors={['transparent', colours.orange]} style={styles.linearGradient}>
-                    <View style={styles.mainContainer}>
-                    <View style={styles.codeContainer}>
-                            <TextInput placeholder='Enter a bar code' style={styles.input}/>
+             <ImageBackground style={styles.backgroundImg} source={barImage}>
+                <LinearGradient colors={['transparent', '#F07719']} style={styles.linearGradient}>
+                
+                <View style={styles.overlayDark} />
+                
+                <View style={styles.rowContainer}>
+                    <Text style={styles.welcome}>Drin</Text><Text style={styles.king}>King</Text>
+                </View>
+
+                <View style={styles.rowContainer}>
+                    <Text style={styles.h2}>Simplifiying your bar experience</Text>
+                </View>
+                
+                <View style={styles.rowContainer}>
+                    <TextInput placeholder='Enter a bar code...' style={styles.input} placeholderTextColor={'#E9E0D6'}/>
+                    <View style={styles.btn}>
+                    <Icon name="check" size={30} color="#E9E0D6"/>
                     </View>
-                    </View>
+                </View>
+                {/* </View> */}
                 </LinearGradient>
             </ImageBackground>
         );
@@ -23,17 +37,47 @@ class WelcomeScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 71,
+        color: "#E9E0D6",
+        top: 124
+      },
+      king: {
+        fontSize: 71,
+        color: "#F07719",
+        top: 124
+      },
+        rowContainer: {
+          flexDirection: 'row',
+          justifyContent: 'center',
+      },
+      h2:{
+        color: '#989490',
+        top: 140,
+    },
     backgroundImg: {
         flex: 1,
         width: null,
         height: null,
       },
     linearGradient: {
-        paddingTop: (Dimensions.get("window").height) / 2,
-        paddingBottom:  (Dimensions.get("window").height / 2) - 30,
-        borderRadius: 5,
+        // paddingTop: (Dimensions.get("window").height) / 2,
+        // paddingBottom:  (Dimensions.get("window").height / 2) - 30,
+        // borderRadius: 5,
         flex: 1,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        top: 0
     },
+    overlayDark: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: "rgba(0,0,0,.6)",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        top: 0
+    },    
     button: {
         fontSize: 36,
         fontFamily: 'Helvetica Neue',
@@ -46,11 +90,13 @@ const styles = StyleSheet.create({
         flexDirection: 'column'
     },
     codeContainer: {
-        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        //   justifyContent: 'center',
+        // justifyContent: 'space-evenly',
         alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row'
+        // width: '100%',
+        // height: '100%',
+        // flexDirection: 'row'
     },
     logoContainer: {
         justifyContent: 'space-evenly',
@@ -60,17 +106,31 @@ const styles = StyleSheet.create({
     },
     input: {
         width: (Dimensions.get("window").width) / 2,
-        height: (Dimensions.get("window").height) / 8,
-        borderWidth: 3,
-        borderRadius: 50,
-        borderColor: colours.white,
-        color: colours.white,
-        fontSize: 30,
-        margin: 10,
-        padding: 10,
+        height: (Dimensions.get("window").height) / 11,
+        borderWidth: 1,
+        // borderRadius: 50,
+        borderColor: '#E9E0D6',
+        color: '#E9E0D6',
+        fontSize: 16,
+        top: 255,
+        // margin: 10,
+        // padding: 10,
         alignSelf: 'flex-start',
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    btn: {
+        width: (Dimensions.get("window").width) / 5,
+        height: (Dimensions.get("window").height) / 11,
+        borderWidth: 1,
+        marginLeft: 10,
+        borderColor: '#E9E0D6',
+        color: '#E9E0D6',
+        // textAlign: 'center',
+        top: 255,
+        alignItems: 'center',
+        justifyContent: 'center'
+        // top: 275
     },
     loginButton: {
         padding: 10,
