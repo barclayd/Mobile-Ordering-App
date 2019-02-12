19bd68821646a029de89e7a1bbfaf9e8f0e05fff
Daniel Barclay <daniel@urbanintel.io>
2019-02-12 19:18:05 +0000

    Added redux to application. Code to support React Debugger and central styles folder for scalable design

--- mobile/src/screens/WelcomeScreen/WelcomeScreen.js
+++ mobile/src/screens/WelcomeScreen/WelcomeScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, Button, StyleSheet, Dimensions, TouchableOpacity, ImageBackground,} from 'react-native';
+import {View, TextInput, StyleSheet, Dimensions, ImageBackground,} from 'react-native';
 import LinearGradient from "react-native-linear-gradient";
-import barImage from '../../assests/barConfetti.jpg';
+import barImage from '../../assets/barConfetti.jpg';
+import * as colours from '../../styles/colourScheme';
 
 class WelcomeScreen extends Component {
 
@@ -9,7 +10,7 @@ class WelcomeScreen extends Component {
 
         return (
             <ImageBackground style={styles.backgroundImg} source={barImage}>
-                <LinearGradient colors={['transparent', '#F07719']} style={styles.linearGradient}>
+                <LinearGradient colors={['transparent', colours.orange]} style={styles.linearGradient}>
                     <View style={styles.mainContainer}>
                     <View style={styles.codeContainer}>
                             <TextInput placeholder='Enter a bar code' style={styles.input}/>
@@ -62,8 +63,8 @@ const styles = StyleSheet.create({
         height: (Dimensions.get("window").height) / 8,
         borderWidth: 3,
         borderRadius: 50,
-        borderColor: '#bebebe',
-        color: '#fff',
+        borderColor: colours.white,
+        color: colours.white,
         fontSize: 30,
         margin: 10,
         padding: 10,
---
+++ mobile/src/styles/colourScheme.js
@@ -0,0 +1,10 @@
+export const grey = '#424242';
+export const cream = '#E9E0D6';
+export const orange = '#F07719';
+export const darkGrey = '#333333';
+export const pink = '#EA61E1';
+export const lightBlue = '#26B9F2';
+export const midnightBlack = '#000d1a';
+export const white = '#ffffff';
+export const lightGreen = '#43C6AC';
+export const purpleBlue = '#191654';
