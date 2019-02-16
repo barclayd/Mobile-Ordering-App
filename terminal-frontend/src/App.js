df2ab097b03de78f46e488ddd1e09318ff71a325
FreddieDev <freddiechessell83@gmail.com>
2019-02-16 09:57:04 +0000

    Created popup UI component and sample customer scanned UI

--- terminal-frontend/src/App.js
+++ terminal-frontend/src/App.js
@@ -2,8 +2,43 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRetweet } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+import PopupWindow from './components/popup-window/popup-window'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      billingPopupVisible: false
+    }
+  }
+
+  showBillingPopup = () => {
+    this.setState({billingPopupVisible: true})
+  }
+  closeBillingPopup = () => {
+    this.setState({billingPopupVisible: false})
+  }
+
+  renderBillingPopup = () => {
+    if (this.state.billingPopupVisible) {
+      return (
+        <PopupWindow className="billingOptions" title="#KHVD PICKUP:" subtitle="Ordered at 11:39pm, 21 minutes ago" showCloseButton={true} closeFunc={this.closeBillingPopup}>
+          <h1>DRINKS:</h1>
+          <div className="indentedContent">
+            <ul className="orderList">
+              <li><span className="quantity">4x</span>VK Orange</li>
+              <li><span className="quantity">1x</span>VK Green</li>
+            </ul>
+
+            <h2>Customer notes:</h2>
+            <p className="indentedPara">pleawse dont put a lime in my Vk becaseu i dont think im not allergic to htem!!!!</p>
+          </div>
+        </PopupWindow>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,6 +57,44 @@ class App extends Component {
           </div>
 
           <h1>AWAITING COLLECTION (2):</h1>
+          <div className="ordersContainer">
+            <div className="orderContainer">
+              <h2>#ALVR - 11:36pm</h2>
+              <div className="orderButtonsContainer">
+                <button className="orderButton">
+                  <span className="icon notReady"></span>
+                  <span className="title">Not ready</span>
+                  <br />
+                  <span className="subtitle">Mark as un-ready</span>
+                </button>
+                <button onClick={this.showBillingPopup} className="orderButton">
+                  <span className="icon notReady"></span>
+                  <span className="title">More</span>
+                  <br />
+                  <span className="subtitle">Billing &amp; more</span>
+                </button>
+              </div>
+            </div>
+            <div className="orderContainer">
+              <h2>#KHVD - 11:39pm</h2>
+              <div className="orderButtonsContainer">
+                <button className="orderButton">
+                  <span className="icon notReady"></span>
+                  <span className="title">Not ready</span>
+                  <br />
+                  <span className="subtitle">Mark as un-ready</span>
+                </button>
+                <button className="orderButton">
+                  <span className="icon notReady"></span>
+                  <span className="title">More</span>
+                  <br />
+                  <span className="subtitle">Billing &amp; more</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <h1>YOUR IN-PROGRESS (2):</h1>
           <div className="ordersContainer">
             <div className="orderContainer">
               <h2>#ALVR - 11:36pm</h2>
@@ -59,12 +132,10 @@ class App extends Component {
             </div>
           </div>
 
-          <h1>YOUR IN-PROGRESS (2):</h1>
-          
           <h3>#BXCQ - 11:41pm</h3>
           <h4>6 orders currently pending...</h4>
 
-
+          { this.renderBillingPopup() }
 
         </header>
       </div>
