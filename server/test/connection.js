7d921e58ea6a6b36bfb853d841f6f8c638f4d5a0
c1673142 <UNORev31410>
2019-02-17 17:29:24 +0000

    29_Add_drinks_to_db: Modified connection.js to remove collections before each test is run.

--- server/test/connection.js
+++ server/test/connection.js
@@ -4,12 +4,17 @@ mongoose.Promise = global.Promise;
 before(function(done){
     //connect to mongoDB
     mongoose.connect('mongodb://localhost/data', { useNewUrlParser: true });
-        // .then(() => console.log('MongoDB Connected...'));
     mongoose.connection.once('open', function () {
         console.log('MongoDB Connected...');
         done();
     }).on('error', function (error) {
         console.log('Connection error: ', error);
+    });
+});
+beforeEach(function(done){
+    //drops collection before each test
+    mongoose.connection.collections.drinks.drop(function(){
+        done();
     });
 });
 after(function(done){
