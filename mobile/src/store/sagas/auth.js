7bd128a12d66d3daf7a170d5d0317213d83cffbc
Daniel Barclay <daniel@urbanintel.io>
2019-02-17 20:35:20 +0000

    Redirect on successful login to ViewDrinks screen. Sign out removes all user sign in info from redux

--- mobile/src/store/sagas/auth.js
+++ mobile/src/store/sagas/auth.js
@@ -1,6 +1,10 @@
 import {put} from 'redux-saga/effects';
 import axios from '../../axios-instance';
+import {Navigation} from "react-native-navigation";
 import * as actions from '../actions/index';
+import IonicIcon from "react-native-vector-icons/Ionicons";
+import {Platform} from "react-native";
+import {setMainApp, setMainAppSettings} from "../../utility/navigation";
 
 export function* authUserSaga(action) {
     yield put(actions.authStart());
@@ -24,10 +28,22 @@ export function* authUserSaga(action) {
         const response = yield axios.post('http://localhost:3000/graphql', JSON.stringify(requestBody));
         if (response.status === 200 && response.status !== 201) {
             yield put(actions.authSuccess(response.data.data.login.token, response.data.data.login.userId, response.data.data.login.tokenExpiration));
+            Promise.all([
+                IonicIcon.getImageSource((Platform.OS === 'android' ? "md-menu" : "ios-menu"), 30),
+                IonicIcon.getImageSource((Platform.OS === 'android' ? "md-person" : "ios-person"), 30)
+            ])
+                .then(sources => {
+                    setMainAppSettings(sources[0], sources[1]);
+                    setMainApp(action.componentId);
+                });
         } else {
             yield put(actions.authFail());
         }
     } catch (err) {
        console.log(err);
     }
+}
+
+export function* logoutSaga(action) {
+    yield put(actions.logoutSucceed());
 }
