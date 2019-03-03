961ee3deff83aec64611d48c12e4529bc451d2e6
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-03-03 00:36:02 +0000

    Re adding drinks Api stuff as was lost in merge

--- mobile/src/screens/ViewDrinks/ViewDrinks.js
+++ mobile/src/screens/ViewDrinks/ViewDrinks.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import {
   View,
+  Text,
   StyleSheet,
+  Image,
   Dimensions,
   ScrollView,
+  TouchableOpacity
 } from "react-native";
 import { connect } from "react-redux";
-import {ThemeProvider} from "react-native-elements";
+import * as colours from "../../styles/colourScheme";
+import { Button, ThemeProvider, SearchBar, Card } from "react-native-elements";
 import ScrollableTabView, {
   DefaultTabBar
 } from "react-native-scrollable-tab-view";
@@ -26,33 +30,41 @@ const theme = {
 class ViewDrinks extends Component {
   state = {
     search: "",
-    drinks: []
+    drinks: [],
+    drinksApi: false
   };
 
   componentDidMount() {
     this.props.findDrinks("beers", this.props.componentId);
   }
 
-  componentWillReceiveProps(nextProps){
-    console.log('nextProps', nextProps)
+  omponentWillReceiveProps(nextProps) {
+    if (!nextProps.loading) {
+      console.log("nextProps", nextProps.data);
+      this.setState({
+        drinks: nextProps.data,
+        drinksApi: true
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    console.log("state view drinks comp", this.state);
   }
 
   render() {
-
     return (
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <View style={styles.background}>
-          <ThemeProvider theme={theme}>
-            <ScrollableTabView
-              style={{ marginTop: 20 }}
-              initialPage={0}
-              renderTabBar={() => <DefaultTabBar />}
-            >
-              <TabScreen2 tabLabel="Beers" />
-              <TabScreen1 tabLabel="Spirits" />
-              <TabScreen3 tabLabel="Wines" />
-            </ScrollableTabView>
-          </ThemeProvider>
+          <ScrollableTabView
+            style={{ marginTop: 20 }}
+            initialPage={0}
+            renderTabBar={() => <DefaultTabBar />}
+          >
+           
+            <TabScreen2 key={1} tabLabel="beers" drinks={this.state.drinks} />
+            
+          </ScrollableTabView>
         </View>
       </ScrollView>
     );
@@ -90,11 +102,13 @@ const mapStateToProps = state => {
     saved: state.drink.saved
   };
 };
-
 const mapDispatchToProps = dispatch => {
   return {
     findDrinks: (category, componentId) =>
       dispatch(actions.findDrinks(category, componentId))
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(ViewDrinks);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ViewDrinks);
