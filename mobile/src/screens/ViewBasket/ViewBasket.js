080577981df548572019b4196af79f3472dde217
Daniel Barclay <barclaysd@me.com>
2019-03-04 21:18:46 +0000

    view basket navigation

---
+++ mobile/src/screens/ViewBasket/ViewBasket.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet} from "react-native";
+
+export class ViewBasket extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default ViewBasket;
+
+const styles = StyleSheet.create({
+
+    
+});
--- mobile/src/utility/screens.js
+++ mobile/src/utility/screens.js
@@ -4,3 +4,4 @@ export const ViewDrinksScreen = 'drinks-app.ViewDrinksScreen';
 export const SideDrawer = 'drinks-app.SideDrawer';
 export const Settings = 'drinks-app.Settings';
 export const ViewMenus = 'drinks-app.ViewMenus'
+export const ViewBasket = 'drinks-app.ViewBasket'
