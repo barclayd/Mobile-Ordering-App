f923ac90db5eb0e17a243af1bd4d60c27aa3d384
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-03-09 05:50:52 +0000

    Ability to customise quantity using longPress.

--- mobile/src/screens/ViewDrinks/TabbedCategories/TabbedCategories.js
+++ mobile/src/screens/ViewDrinks/TabbedCategories/TabbedCategories.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import {
   Text,
   View,
-  TouchableOpacity,
+  TouchableHighlight,
   StyleSheet,
   Dimensions,
 } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {Card} from "react-native-elements";
 import OverlayComponent from "../../../components/UI/Backgrounds/Overlay/OverlayComponent";
+import * as colours from "../../../styles/colourScheme"
+import {connect} from 'react-redux';
 
 class TabbedCategories extends Component {
   state = {
     isVisible: false,
-    drinkSelected: {}
+    drinkSelected: {},
+    trashCanVisible: false,
+    itemSelected: "",
+    quantity: 0
   };
 
   openOverlay = (i) => {
@@ -36,6 +42,33 @@ class TabbedCategories extends Component {
     });
   };
 
+  _onLongPressButton = (i,u) => {
+    console.log("this.props.basket",this.props.basket)
+    // let basketItems = []
+  //   this.props.basket.map(drink => {
+  //     basketItems.push({name: drink.name, quantity: drink.quantity})
+  // });
+  // console.log("basketItems",basketItems)
+    this.setState({
+      trashCanVisible: !this.state.trashCanVisible,
+      itemSelected: u.name,
+      // basketItems: basketItems
+    });
+  }
+
+  componentDidUpdate(){
+    console.log(this.state.trashCanVisible," Trash can vis")
+  }
+
+  basketItems = (name) => {
+    let totalItems = 0;
+    this.props.basket.map(drink => {
+        if (drink.name == name)
+          totalItems += drink.quantity
+      });
+    return totalItems;
+    };
+
 
   render() {
     console.log(this.props.category);
@@ -43,17 +76,37 @@ class TabbedCategories extends Component {
       <View>
         {this.props.drinks.length > 0 ? this.props.drinks.map((u, i) => {
           return (
-            <TouchableOpacity key={i} onPress={() => this.openOverlay(i)}>
+            <TouchableHighlight key={i} onPress={() => this.openOverlay(i)}  onLongPress={()=> this._onLongPressButton(i,u)} underlayColor="white">
               <Card>
                 <View style={styles.rowContainer}>
+
                   <View style={styles.leftContainer}>
                     <Text style={styles.name}>{u.name}</Text>
                   </View>
+
+                  
+                  <View>
                   <Text style={styles.price}>£{u.price}</Text>
-                </View>
-                <Text style={styles.description}>{u.nutritionInfo}</Text>
+                  </View>
+                  </View>
+
+                  <Text style={styles.description}>{u.nutritionInfo}</Text>
+
+                  <View style={styles.trashContainer}>
+
+                  {this.state.trashCanVisible &&  this.props.drinks[i].name == this.state.itemSelected ? 
+                  <Icon name="trash-o" style={styles.trash} size={30} color={colours.orange}/> 
+                  : null}
+
+                  {this.state.trashCanVisible && this.props.drinks[i].name == this.props.basket.map(drink => drink.name) && this.props.drinks[i].name == this.state.itemSelected ?
+                  <Text style={styles.price}>x{this.basketItems(u.name)}</Text>
+                  :null}
+
+                  </View>
+                
+                
               </Card>
-            </TouchableOpacity>
+            </TouchableHighlight>
           );
         }):null}
         <OverlayComponent
@@ -68,6 +121,9 @@ class TabbedCategories extends Component {
 }
 
 const styles = StyleSheet.create({
+  trash: {
+    marginRight: 5
+  },
   slidingTabs: {
     flex: 1,
     top: 0,
@@ -82,13 +138,6 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0
-  },
-  leftContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    paddingBottom: "1%"
-    // backgroundColor: 'green'
   },
   price: {
     fontWeight: "600",
@@ -105,6 +154,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between"
   },
+  trashContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
   title: {
     paddingTop: 10,
     paddingLeft: Dimensions.get("window").width / 14,
@@ -113,4 +166,11 @@ const styles = StyleSheet.create({
   contentContainer: {}
 });
 
-export default TabbedCategories;
+const mapStateToProps = state => {
+  return {
+      basket: state.basket.basket,
+      basketCategories: state.basket.categories
+  }
+};
+
+export default connect(mapStateToProps, null)(TabbedCategories)
