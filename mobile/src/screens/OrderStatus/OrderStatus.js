98b78ab1ca865fec85a7ebe03dd8af2df5e8f68b
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-03-14 18:10:56 +0000

    Order status page, added react-native-progress, therefore needs fresh re-install.

--- mobile/src/screens/OrderStatus/OrderStatus.js
+++ mobile/src/screens/OrderStatus/OrderStatus.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import * as colours from '../../styles/colourScheme';
 import {Navigation} from "react-native-navigation";
+import * as Progress from 'react-native-progress';
+
 
 class OrderStatus extends Component {
 
@@ -9,6 +11,19 @@ class OrderStatus extends Component {
         super(props);
         Navigation.events().bindComponent(this); // <== Will be automatically unregistered when unmounted
     }
+
+    componentDidMount(){
+        console.log("mounted");
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps,"nextProps")
+        // if (!nextProps.loading) {
+        //   this.setState({
+        //     pastOrders: nextProps.pastOrders
+        //   });
+        // }
+      }
 
     navigationButtonPressed({ buttonId }) {
         if (buttonId === "menuButton") {
@@ -41,19 +56,75 @@ class OrderStatus extends Component {
         return (
 
             <View style={[styles.container]}>
-                <View style={styles.containerText}>
+
+                <View style={styles.header}>
+                <Text style={styles.status}>Order Successful</Text>
+                <Text style={styles.success}>Thank You Guy for you order!</Text>
+                <Text style={styles.orderText}>Order Number : {this.props.orderNumber}</Text>
+                <View style={styles.progressCircle}>
+                <Text style={styles.orderText}>Status: Pending... </Text>
+                <Progress.Circle size={30} indeterminate={true} color={colours.orange} thickness={15}/>
+                </View>
+                <Text style={styles.orderText}>Estimated Collection Time </Text>
+                </View>
+
+                {/* <View style={styles.box}>
+                <Text style={styles.status}>In Progress...</Text>
+                </View>
+                
+
+                <View style={styles.orderInfo}>
                     <Text style={styles.text}> Order Number </Text>
-                     <Text style={styles.text}> Estimated Collection Time </Text>
+                     
                 <TouchableOpacity>
                         <Text style={styles.text}> View QR Code </Text>
                     </TouchableOpacity>
+                </View> */}
                 </View>
-            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    progressCircle:{
+    flexDirection: "row",
+    justifyContent: 'flex-start',
+    alignItems: "center",
+    },
+    box:{
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: "center",
+        paddingTop: 10,
+        marginRight: 10,
+        marginLeft: 10,
+        borderColor: colours.cream,
+        borderWidth: 2
+    },
+    orderInfo:{
+    paddingTop: 10,
+    paddingRight: 10,
+    },
+    status:{
+      fontSize: 24,
+      fontWeight: '600',
+      color: colours.cream,
+      padding: 10,
+    },
+    success:{
+        fontSize: 20,
+        fontWeight: '500',
+        color: colours.cream,
+        padding: 10,
+        color: colours.orange,
+        marginBottom: 25,
+      },
+    orderText:{
+        fontSize: 16,
+        fontWeight: '400',
+        color: colours.cream,
+        padding: 10,
+      },
     container: {
         paddingTop: 22,
         backgroundColor: colours.midnightBlack,
