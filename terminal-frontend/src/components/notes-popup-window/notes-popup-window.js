55af68827c762cb4fb4286c71a7f8af518d224db
FreddieDev <freddiechessell83@gmail.com>
2019-03-21 17:09:02 +0000

    Made all components that don't use state into functional components, drastically improving performance and page load time

--- terminal-frontend/src/components/notes-popup-window/notes-popup-window.js
+++ terminal-frontend/src/components/notes-popup-window/notes-popup-window.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './style.css'
-import PopupWindow from '../popup-window/popup-window'
-import TimeAgo from '../time-ago-clean/time-ago-clean'
+import PopupWindow from '../../containers/popup-window/popup-window'
+import TimeAgo from '../../containers/time-ago-clean/time-ago-clean'
 import { DateTime } from 'luxon'
 
-export default class NotesPopupWindow extends React.Component {
-
+const NotesPopupWindow = (props) => {
+    
     // Time formatting with Luxon: https://moment.github.io/luxon/docs/manual/formatting.html#table-of-tokens
-    buildSubtitle = (order) => {
+    const buildSubtitle = (order) => {
         if (order) return (
             <span>
                 for <span className="orderID">#{order.id}</span> ordered at {DateTime.fromJSDate(order.orderDate).toFormat("h:mma")},&nbsp;
@@ -17,26 +17,26 @@ export default class NotesPopupWindow extends React.Component {
         ); else return (<span></span>);
     }
     
-    buildNotes = (order) => {
+    const buildNotes = (order) => {
         if (order) return (<p className="notesPara">{order.notes}</p>); else return "";
     }
 
-    render () {
-        return (
-            <PopupWindow
-                    className="notes"
-                    title={"Customer notes:"}
-                    subtitle={this.buildSubtitle(this.props.order)}
-                    showCloseButton={true}
-                    showFunc={this.props.showFunc}
-            >
-                {this.buildNotes(this.props.order)}
-            </PopupWindow>
-        )
-    }
+    return (
+        <PopupWindow
+                className="notes"
+                title={"Customer notes:"}
+                subtitle={buildSubtitle(props.order)}
+                showCloseButton={true}
+                showFunc={props.showFunc}
+        >
+            {buildNotes(props.order)}
+        </PopupWindow>
+    )
 }
 
 NotesPopupWindow.propTypes = {
     order: PropTypes.object,
     showFunc: PropTypes.func.isRequired, // Callback function held in parent that calls popup window instance's ShowPopup()
 }
+
+export default NotesPopupWindow;
--- terminal-frontend/src/components/switch-accounts-popup-window/switch-accounts-popup-window.js
+++ terminal-frontend/src/components/switch-accounts-popup-window/switch-accounts-popup-window.js
@@ -2,37 +2,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './style.css'
-import PopupWindow from '../popup-window/popup-window'
+import PopupWindow from '../../containers/popup-window/popup-window'
 
-export default class SwitchAccountsPopupWindow extends React.Component {
-    render () {
-        return (
-            <PopupWindow
-                    className="switchAccountsPopup"
-                    title="Switch account:"
-                    subtitle={<span>Select your username to add to the hotbar</span>}
-                    showCloseButton={true}
-                    showFunc={this.props.showFunc}
-            >
-                <div className="staffMembersButtonContainer">
-                    {
-                        this.props.staffMembers.map((staffMember) => {
-                            return (
-                                <button key={staffMember.id} className="staffMemberButton">
-                                    {staffMember.firstName}
-                                    <br/>
-                                    {staffMember.surname}
-                                </button>
-                            )
-                        })
-                    }
-                </div>
-            </PopupWindow>
-        )
-    }
+const SwitchAccountsPopupWindow = (props) => {
+
+    return (
+        <PopupWindow
+                className="switchAccountsPopup"
+                title="Switch account:"
+                subtitle={<span>Select your username to add to the hotbar</span>}
+                showCloseButton={true}
+                showFunc={props.showFunc}
+        >
+            <div className="staffMembersButtonContainer">
+                {
+                    props.staffMembers.map((staffMember) => {
+                        return (
+                            <button key={staffMember.id} className="staffMemberButton">
+                                {staffMember.firstName}
+                                <br/>
+                                {staffMember.surname}
+                            </button>
+                        )
+                    })
+                }
+            </div>
+        </PopupWindow>
+    )
 }
 
 SwitchAccountsPopupWindow.propTypes = {
     staffMembers: PropTypes.array,
     showFunc: PropTypes.func, // Callback function held in parent that calls popup window instance's ShowPopup()
 }
+
+export default SwitchAccountsPopupWindow;
