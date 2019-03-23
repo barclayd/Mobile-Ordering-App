8d3c1d5495e3a3511196ee02de295a98fc7175f2
FreddieDev <freddiechessell83@gmail.com>
2019-03-23 12:03:07 +0000

    Added enums to support new mongoose queries

--- terminal-frontend/src/OrderStatuses.js
+++ terminal-frontend/src/OrderStatuses.js
@@ -1,7 +1,11 @@
 const OrderStatuses = {
     AWAITING_COLLECTION: 0,
     IN_PROGRESS: 1,
-    PENDING: 2
+    PENDING: 2,
+    AWAITING_USER_DECISION: 3,
+    COMPLETED: 4,
+    REFUNDED: 5,
+    CANCELLED: 6,
 };
 
 export default OrderStatuses;
