a28816c712e26dc177bedc807a491614f6132a33
Daniel Barclay <barclaysd@me.com>
2019-03-26 23:14:22 +0000

    Smooth animations when clicking between places on map

--- mobile/src/components/MapDisplay/MapDisplay.js
+++ mobile/src/components/MapDisplay/MapDisplay.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
 import MapView from 'react-native-maps';
 
 
-const mapDisplay = props => {
+class MapDisplay extends Component {
 
-    return (
-        <View style={styles.container}>
-        <MapView
-            style={styles.map}
-            initialRegion={{
-                latitude: 51.4816,
-                longitude: -3.1791,
-                latitudeDelta: 0.0122,
-                longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.122
-            }}
-        />
-        </View>
-    )
-};
+    state = {
+        focusedLocation: {
+            latitude: 51.4816,
+            longitude: -3.1791,
+            latitudeDelta: 0.0122,
+            longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.122
+        }
+    };
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            ...this.state.focusedLocation,
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        });
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                }
+            }
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={this.state.focusedLocation}
+                    onPress={this.pickLocationHandler}
+                    ref={ref => this.map = ref}
+                />
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -35,4 +61,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default mapDisplay;
+export default MapDisplay;
