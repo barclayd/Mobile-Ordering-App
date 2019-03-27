82c226470e1d43b4108f4892f61ac0f8393e5ec9
Daniel Barclay <barclaysd@me.com>
2019-03-27 20:13:01 +0000

    customiser: Added react router.

---
+++ customiser_web-app/src/components/drinks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class drinksEdit extends Component {
+  render() {
+    return (
+      <div><h1>drinksEdit Page</h1></div>
+    )
+  }
+}
+
+export default drinksEdit;
---
+++ customiser_web-app/src/components/statistics.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class statistics extends Component {
+  render() {
+    return (
+      <div><h1>Stats Page</h1></div>
+    )
+  }
+}
+
+export default statistics;
