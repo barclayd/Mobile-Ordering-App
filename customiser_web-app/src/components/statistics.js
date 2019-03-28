3b16dc4c629f1b0f2b1b36aa1b22363867ed5d16
Daniel Barclay <barclaysd@me.com>
2019-03-28 00:18:21 +0000

    customiser: basic router navigation working.

--- customiser_web-app/src/components/statistics.js
+++ customiser_web-app/src/components/statistics.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
-class statistics extends Component {
+export default class statistics extends Component {
   render() {
-    return (
-      <div><h1>Stats Page</h1></div>
+    return(
+      <div><h1>Stats page</h1></div>
     )
   }
 }
-
-export default statistics;
