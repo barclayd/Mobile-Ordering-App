11684d8a97446ded4d2709571fa0a7db78693993
c1673142 <UNORev31410>
2019-03-28 11:31:42 +0000

    customiser: Linked account page.

--- customiser_web-app/src/components/main.js
+++ customiser_web-app/src/components/main.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import main_page from './main_page';
 import statistics from './statistics';
 import Menu from './menu';
+import account from './account';
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={main_page} />
     <Route path="/menu" component={Menu} />
     <Route path="/statistics" component={statistics} />
+    <Route path="/account" component={account} />
   </Switch>
 );
 
