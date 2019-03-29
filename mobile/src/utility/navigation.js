1e8a101899e0e592a5707629f07a6d238d938b01
Daniel Barclay <barclaysd@me.com>
2019-03-29 00:00:03 +0000

    Fixed navigation issues from merge into master

--- mobile/src/utility/navigation.js
+++ mobile/src/utility/navigation.js
@@ -139,25 +139,6 @@ const setMainApp = async (componentId, barName, barCode) => {
     });
 };
 
-const setViewDrinks = async (componentId, menuName, menuId) => {
-    await Navigation.push(componentId, {
-        component: {
-            name: screens.ViewDrinksScreen,
-            passProps: {
-                authState: menuName,
-                menuId
-            },
-            options: {
-                topBar: {
->>>>>>> feature/252_dynamic_menus
-                    visible: true,
-                    style: 'light'
-                }
-            }
-        }
-    });
-};
-
 const setOrderStatus = async (componentId, collectionId, userId, collectionPoint, date, orderNumber) => {
     let component = componentId;
     if (!componentId) {
