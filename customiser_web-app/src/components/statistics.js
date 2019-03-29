a979ef65355b95742df1133d86f178927f4f4868
c1673142 <UNORev31410>
2019-03-29 17:26:43 +0000

    customiser: Added dependencies

--- customiser_web-app/src/components/statistics.js
+++ customiser_web-app/src/components/statistics.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+import fusioncharts from 'fusioncharts';
+import charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import theme from 'fusioncharts/themes/fusioncharts.theme.fint';
+
+//passing these as dependencies
+charts(fusioncharts);
+theme(fusioncharts);
+
 export default class statistics extends Component {
   render() {
     return(
