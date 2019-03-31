1a9cc5c96ff592aa1d21f0c5378e281996d08edf
Daniel Barclay <barclaysd@me.com>
2019-03-31 17:48:13 +0100

    customiser: Added some boxes for user initial set Up.

--- customiser_web-app/src/components/setUp.js
+++ customiser_web-app/src/components/setUp.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 
 export default class setUp extends Component {
+  constructor(props) {
+    super(props);
+
+    const typeOfBar = [
+      {
+        name: "Student Bar",
+        description: "Suitable for bars for students."
+      },
+      {
+        name: "Cocktail Bar",
+        description: "Suitable for cocktail bars."
+      },
+      {
+        name: "Festivals",
+        description: "Suitable for festivals."
+      },
+    ];
+    this.state = {current:0, barSelect: typeOfBar}
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(select) {
+    if (this.state.current == 4) {
+      this.setState({current: 0})
+    }
+    else{
+      this.setState({current: this.state.current + 1})
+    }
+  }
   render() {
     return(
-      <div><h1>Set Up application page</h1></div>
+      <div>
+        <selectionBox handleClick={this.handleClick} dataSet={this.state.dataSet[this.state.current]} />
+      </div>
     )
   }
 }
+
+function selectionBox(props) {
+  let style = {
+    width: "25%",
+    display: "block",
+    textAlign: "center",
+    boxSizing: "border-box",
+    float: "left",
+    padding: "0 2em"
+  }
