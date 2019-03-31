82f94daf03e80f25fe865df1bab4088baf07cf93
c1673142 <UNORev31410>
2019-03-31 16:00:17 +0100

    customiser: Added setup

--- customiser_web-app/src/components/main_page.js
+++ customiser_web-app/src/components/main_page.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export default class main_page extends Component {
   render() {
     return(
-      <div><h1>Main Page</h1></div>
+      <div><h1>Main Page</h1>
+      <p>would you like to <Link to="/setUp">set up?</Link></p></div>
     )
   }
 }
