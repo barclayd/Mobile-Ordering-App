94e3c6565af0a0f92d9de2453598bef6e7c44c8d
Daniel Barclay <barclaysd@me.com>
2019-04-01 23:09:46 +0100

    Redux added to support new orders retrieved via GraphQL subscriptions

--- terminal/src/store/sagas/index.js
+++ terminal/src/store/sagas/index.js
@@ -1,11 +1,12 @@
 import {takeLatest, takeEvery} from 'redux-saga/effects';
-import {getOrdersByCollectionPointSaga, updateOrderSaga} from './order';
+import {getOrdersByCollectionPointSaga, updateOrderSaga, newOrderSaga} from './order';
 import {findBarStaffSaga} from './bar';
 import * as actionTypes from '../actions/actionTypes';
 
 export function* watchOrders() {
     yield takeLatest(actionTypes.GET_ORDERS_BY_COLLECTION_POINT, getOrdersByCollectionPointSaga);
-    yield takeEvery(actionTypes.UPDATE_ORDER, updateOrderSaga); // Use takeEvery here so concurrent order updates will be written to state
+    yield takeEvery(actionTypes.UPDATE_ORDER, updateOrderSaga);
+    yield takeEvery(actionTypes.NEW_ORDER, newOrderSaga);// Use takeEvery here so concurrent order updates will be written to state
 }
 
 export function* watchBar() {
