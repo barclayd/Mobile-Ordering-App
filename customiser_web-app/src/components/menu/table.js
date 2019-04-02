5a8304e6c83ffc160fdf570779dcd865843ec420
Daniel Barclay <barclaysd@me.com>
2019-04-02 04:08:09 +0100

    customiser: Added table template and basic hardcoded crud functions.

--- customiser_web-app/src/components/menu/table.js
+++ customiser_web-app/src/components/menu/table.js
@@ -1,1 +1,82 @@
 import React from "react";
+import { Table } from "material-ui/Table";
+import Editable from "./editable";
+
+//code adapted from : https://github.com/benawad/basic-react-form/blob/8_cancel_edit/src/Table.js
+
+const row = (
+  x,
+  i,
+  header,
+  handleRemove,
+  startEditing,
+  editIdx,
+  handleSave,
+  stopEditing
+) => {
+  const currentlyEditing = editIdx === i;
+  return currentlyEditing ? (
+    <TableRow key={`inline-form-${i}`} selectable={false}>
+      <InlineForm
+        handleSave={handleSave}
+        header={header}
+        x={x}
+        i={i}
+        stopEditing={stopEditing}
+      />
+    </TableRow>
+  ) : (
+    <TableRow key={`tr-${i}`} selectable={false}>
+      {header.map((y, k) => (
+        <TableRowColumn key={`trc-${k}`}>{x[y.prop]}</TableRowColumn>
+      ))}
+      <TableRowColumn>
+        <EditIcon onClick={() => startEditing(i)} />
+        <TrashIcon onClick={() => handleRemove(i)} />
+      </TableRowColumn>
+    </TableRow>
+  );
+};
+
+export default ({
+                  data,
+                  header,
+                  handleRemove,
+                  startEditing,
+                  editIdx,
+                  handleSave,
+                  stopEditing,
+                }) => (
+  <Table>
+    <TableHeader>
+      <TableRow>
+        {header.map((x, i) => (
+          <TableHeaderColumn key={`thc-${i}`}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center"
+              }}
+            >
+            </div>
+          </TableHeaderColumn>
+        ))}
+        <TableHeaderColumn />
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {data.map((x, i) =>
+        row(
+          x,
+          i,
+          header,
+          handleRemove,
+          startEditing,
+          editIdx,
+          handleSave,
+          stopEditing
+        )
+      )}
+    </TableBody>
+  </Table>
+);
