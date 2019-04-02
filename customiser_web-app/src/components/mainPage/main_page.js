e97538f5ae79fd6fc5cc41d0892ec1701ea43e77
c1673142 <UNORev31410>
2019-04-02 13:34:42 +0100

    customiser: Added DrinKing logo, and added some more styling

--- customiser_web-app/src/components/mainPage/main_page.js
+++ customiser_web-app/src/components/mainPage/main_page.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Grid, Cell } from 'react-mdl';
-import './main_page.css'
+import logo from '../../assets/Logo.png';
+import './main_page.css';
 
 export default class main_page extends Component {
   render() {
@@ -9,17 +10,13 @@ export default class main_page extends Component {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Grid className="main-grid">
           <Cell col={12}>
-            <img
-              src='../../../../mobile/src/assets/Logo.png'
-              alt="logo"
-              className="avatar-img"
-            />
+
 
             <div className="main">
+              <img src={logo} width="10%" height="10%"/>;
               <h1>DrinKing Customiser Application</h1>
               <p>The Main page for the DrinKing customiser application. Here you can
               <Link to="/setUp"> set up?</Link></p>
-
             </div>
           </Cell>
         </Grid>
