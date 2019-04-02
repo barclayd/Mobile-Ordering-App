396a7ca6ee926b5061ba0422505be64b86480975
c1673142 <UNORev31410>
2019-04-02 05:19:35 +0100

    customiser: Added some styling.

--- customiser_web-app/src/components/mainPage/main_page.js
+++ customiser_web-app/src/components/mainPage/main_page.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { Grid, Cell } from 'react-mdl';
+import './main_page.css'
 
 export default class main_page extends Component {
   render() {
-    return(
-      <div><h1>Main Page</h1>
-      <p>would you like to <Link to="/setUp">set up?</Link></p></div>
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src='../../../../mobile/src/assets/Logo.png'
+              alt="logo"
+              className="avatar-img"
+            />
+
+            <div className="main">
+              <h1>DrinKing Customiser Application</h1>
+              <p>The Main page for the DrinKing customiser application. Here you can
+              <Link to="/setUp"> set up?</Link></p>
+
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
