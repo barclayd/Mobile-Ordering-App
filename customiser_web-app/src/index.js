14377f7181164bc50e5d23d361004e7017007e63
Daniel Barclay <barclaysd@me.com>
2019-04-02 17:41:44 +0100

    customiser: Added return

--- customiser_web-app/src/index.js
+++ customiser_web-app/src/index.js
@@ -5,7 +5,27 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import ordersReducer from "../../terminal/src/store/reducers/order";
+
+const rootReducer = combineReducers({
+  orders: ordersReducer
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
   <BrowserRouter>
