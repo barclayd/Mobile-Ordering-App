5d6f8e51a851f3e581b5983a15902f2492ee5fc1
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-04-02 15:45:24 +0100

    localhost on master

--- mobile/src/axios-instance.js
+++ mobile/src/axios-instance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'http://localhost:3000/graphql',
     // baseURL: 'http://192.168.0.15:3000/graphql',
-    baseURL: 'http://172.20.10.6:3000/graphql',
+    baseURL: 'http://localhost:3000/graphql',
     headers: {
             'Content-Type': 'application/json'
     }
