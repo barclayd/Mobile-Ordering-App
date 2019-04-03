49fd389a88943182d12a893bedf39919bc56400d
c1673142 <UNORev31410>
2019-04-03 23:52:11 +0100

    customiser: got router for stats pages sorted.

--- customiser_web-app/src/components/main.js
+++ customiser_web-app/src/components/main.js
@@ -5,7 +5,9 @@ import main_page from './mainPage/main_page';
 import statistics from './statistics/statistics';
 import Menu from './menu/menu';
 import account from './account/account';
-import setUp from './initialSetUp/setUp'
+import setUp from './initialSetUp/setUp';
+import WHSmith from './statistics/WHSmith';
+import TopFloor from './statistics/TopFloor';
 
 const Main = () => (
   <Switch>
@@ -14,6 +16,8 @@ const Main = () => (
     <Route path="/statistics" component={statistics} />
     <Route path="/account" component={account} />
     <Route path="/setUp" component={setUp} />
+    <Route path="/whsmith" component={WHSmith} />
+    <Route path="/topfloor" component={TopFloor} />
   </Switch>
 );
 
