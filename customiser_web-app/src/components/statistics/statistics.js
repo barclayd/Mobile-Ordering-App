75b4d15b8b2cc842333cbc3d9ab2d63f7f9eea77
c1673142 <UNORev31410>
2019-04-03 23:26:19 +0100

    customiser: just need to add json to axis.

--- customiser_web-app/src/components/statistics/statistics.js
+++ customiser_web-app/src/components/statistics/statistics.js
@@ -6,28 +6,22 @@ import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import ReactFC from 'react-fusioncharts';
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
-
-// export default class statistics extends React.Component {
+export default class statistics extends React.Component {
+  render () {
+    return <ReactFC {...chartConfigs} />;
+  }
+}
 
   const dataSource = {
     chart: {
-      caption: 'Countries With Most Oil Reserves [2017-18]',
-      subCaption: 'In MMbbl = One Million barrels',
-      xAxisName: 'Country',
-      yAxisName: 'Reserves (MMbbl)',
-      numberSuffix: 'K',
+      caption: 'Number of orders',
+      subCaption: 'In WHSMiths',
+      xAxisName: 'Order Point',
+      yAxisName: 'Number of orders',
+      numberSuffix: '',
       theme: 'fusion'
     },
-    data: [
-      { label: 'Venezuela', value: '290' },
-      { label: 'Saudi', value: '260' },
-      { label: 'Canada', value: '180' },
-      { label: 'Iran', value: '140' },
-      { label: 'Russia', value: '115' },
-      { label: 'UAE', value: '100' },
-      { label: 'US', value: '30' },
-      { label: 'China', value: '30' }
-    ]
+    data: this.props.orders
   };
 //}
 
@@ -38,11 +32,7 @@ ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
     dataFormat: 'json',
     dataSource: dataSource
   };
-export default class statistics extends React.Component {
-  render () {
-    return <ReactFC {...chartConfigs} />;
-  }
-}
+
 const mapStateToProps = state => {
   return {
     orders: state.orders.orders,
