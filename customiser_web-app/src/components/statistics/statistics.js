e1433dbbf1ff5210c6446c26f96275edb6b1bb77
c1673142 <UNORev31410>
2019-04-04 12:58:22 +0100

    customiser: Basic server implementation.

--- customiser_web-app/src/components/statistics/statistics.js
+++ customiser_web-app/src/components/statistics/statistics.js
@@ -1,54 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as actions from "../../store/actions";
+import { connect } from "react-redux";
 import FusionCharts from 'fusioncharts';
 import Charts from 'fusioncharts/fusioncharts.charts';
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import ReactFC from 'react-fusioncharts';
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
-export default class WHSmith extends React.Component {
+
+class statistics extends React.Component {
+  // componentDidMount() {
+  //   this.props.FindOrdersByCollectionPoint()
+  // }
   render () {
     return <ReactFC className="chart" {...chartConfigs} />;
   }
 }
 
-const WHSmithData = {
+
+
+const data = {
   chart: {
     caption: 'Number of orders',
-    subCaption: 'In WHSMiths',
+    subCaption: 'the Taf',
     xAxisName: 'Order Point',
     yAxisName: 'Number of orders',
     numberSuffix: '',
     theme: 'fusion'
   },
   data: [
-    { label: 'WHSmith', value: '260' },
-    { label: 'Top Floor', value: '180' },
-
+    { label: 'WHSmith', value: '4' },
+    { label: 'Top Floor', value: '2' },
   ]
 };
 
 const chartConfigs = {
   type: 'column2d',
-  width: 600,
-  height: 400,
+  width: 400,
+  height: 300,
   dataFormat: 'json',
-  dataSource: WHSmithData
+  dataSource: data
 };
-//
-// const mapStateToProps = state => {
-//   return {
-//     orders: state.orders.orders,
-//     loading: state.orders.loading
-//   };
-// };
-//
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     FindOrdersByCollectionPoint: (collectionPoint) => dispatch(actions.getOrdersByCollectionPoint(collectionPoint)),
-//   };
-// };
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(statistics);
 
-ReactDOM.render(<ReactFC {...chartConfigs} />, document.getElementById('root'));
+const mapStateToProps = state => {
+  return {
+    orders: state.orders.orders,
+    loading: state.orders.loading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    FindOrdersByCollectionPoint: (collectionPoint) => dispatch(actions.getOrdersByCollectionPoint(collectionPoint)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(statistics);
