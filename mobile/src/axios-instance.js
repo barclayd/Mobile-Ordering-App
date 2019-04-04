d1cf5bb7c1f0d495fe58e84b732da5230fe4a572
Daniel Barclay <barclaysd@me.com>
2019-04-04 23:43:10 +0100

    Fixed axios-instance to connect to server

--- mobile/src/axios-instance.js
+++ mobile/src/axios-instance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `http://localhost` + `${process.env.PORT || 3000}/graphql`,
-    // baseURL: 'http://192.168.0.15:3000/graphql',
+    baseURL: `http://localhost:3000/graphql`,
     headers: {
             'Content-Type': 'application/json'
     }
